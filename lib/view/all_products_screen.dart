06928d1fbc9e19ba39be7efbc31ac50c1bc42237
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2025-09-20 16:27:09 +0530

    part 8 complete

--- lib/view/all_products_screen.dart
+++ lib/view/all_products_screen.dart
@@ -1,30 +1,38 @@
 import 'package:ecommerce_app/utils/app_text_styles.dart';
-import 'package:ecommerce_app/view/widgets/category_chips.dart';
 import 'package:ecommerce_app/view/widgets/filter_bottom_sheet.dart';
 import 'package:ecommerce_app/view/widgets/product_grid.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class ShoppingScreen extends StatelessWidget {
-  const ShoppingScreen({super.key});
+class AllProductsScreen extends StatelessWidget {
+  const AllProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final isDark = Theme.of(context).brightness==Brightness.dark;
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
-        title: Text('Shopping',
-          style: AppTextStyles.withColor(
-              AppTextStyles.h3,
-              isDark ? Colors.white: Colors.black,),
+        leading: IconButton(
+            onPressed: () => Get.back(),
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: isDark ? Colors.white : Colors.black,
+            ),
+        ),
+        title: Text('All Products',
+        style: AppTextStyles.withColor(
+            AppTextStyles.h3,
+            isDark ? Colors.white : Colors.black,
+        ),
         ),
         actions: [
           // search icon
           IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.search,
-              color:  isDark ? Colors.white : Colors.black,
-            ),
+              onPressed: (){},
+              icon: Icon(Icons.search,
+                color:  isDark ? Colors.white : Colors.black,
+              ),
           ),
           //filter icon
           IconButton(
@@ -35,16 +43,7 @@ class ShoppingScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 16),
-            child: CategoryChips(),
-          ),
-          Expanded(child: ProductGrid(),
-          ),
-        ],
-      ),
+      body: const ProductGrid(),
     );
   }
 }
